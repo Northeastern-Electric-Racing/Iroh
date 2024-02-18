f23f16171445ffb1a71ec5587da0b8a2ed15d948
caiodasilva2005 <145620095+caiodasilva2005@users.noreply.github.com>
2024-02-18 17:38:39 -0500

    Added functionality to receiving CAN IDs (#21)

--- Core/Src/can.c
+++ Core/Src/can.c
@@ -1,34 +1,43 @@
-#include "can.h";
+#include "can.h"
+#include "queues.h"
 
 //Arbitrary CAN IDs
-#define VOLTAGE_CAN_ID 10
-#define CURRENT_CAN_ID 20
+#define CAN_ID 0x69
+
+bms_data_t bms_data;
+
+
+void init_iroh_can(FDCAN_HandleTypeDef *hcan) {
+    can_t fdcan;
+    fdcan.callback = receive_msg;
+    fdcan.id_list = 0;
+    fdcan.id_list_len = 0;
+    fdcan.hcan = hcan;
+
+    can_init(&fdcan);
+}    
 
 HAL_StatusTypeDef receive_msg(can_t *can) 
 {
 
     FDCAN_RxHeaderTypeDef rx_header;
-    uint8_t rx_data[8]; //array of 8 bytes 
+    uint8_t rx_data[8]; //array of 8 bytes
 
     if (HAL_FDCAN_GetRxFifoFillLevel(can->hcan, FDCAN_RX_FIFO0) > 0) { //Check if messages are available
         if (HAL_FDCAN_GetRxMessage(can->hcan,FDCAN_RX_FIFO0,&rx_header,rx_data) == HAL_OK) {
             uint32_t message_id = rx_header.Identifier;
-            if (message_id == VOLTAGE_CAN_ID) {
-				//process voltage ID
+            if (message_id == CAN_ID) {
+                bms_data.SoC = (uint8_t) rx_data[0];
+                bms_data.current = (uint8_t) rx_data[1];
+                if (osStatus_t osMessageQueuePut(bms_data_queue *bms_data, 0, 2000) == osOK) //timeout is 2000 ms
+                    return HAL_OK;
+                else
+                    return HAL_ERROR
 			}
-			if (message_id == CURRENT_CAN_ID) {
-				//process current ID
-			}
-			return HAL_OK;
         }
 		return HAL_ERROR;
     } 
 	return HAL_BUSY;
 }
 
-//initialize can
-can_t fdcan;
-fdcan.callback = receive_msg;
-
-can_init(&fdcan);    
 
